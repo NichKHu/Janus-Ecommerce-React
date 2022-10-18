83e57766e1c66a55092e5951944e2b8f83f738ff
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-17 23:27:31 -0700

    Added navbar component and adjusted dropdown

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Navbar.css';
+import {AiOutlineUser} from 'react-icons/ai';
+import {AiOutlineSearch} from 'react-icons/ai';
+import {SlBag} from 'react-icons/sl';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <div className='dropdown-main'>
+          <ul>
+            <li><a href='#'>Collections</a></li>
+            <li><a href='#'>Bracelets</a></li>
+            <li><a href='#'>Rings</a></li>
+            <li><a href='#'>Necklaces</a></li>
+            <li><a href='#'>Women's</a></li>
+            <li><a href='#'>Custom Made</a></li>
+            <li><a href='#'>Gift Guide</a></li>
+          </ul>
+        </div>
+        <div className='user'>
+          <div className='dropdown'>
+            <button className='dollar-conversion'>USD</button>
+            <div className='dropdown-menu'>
+              Dropdown Content
+            </div>
+          </div>
+          <span className='user-profile'><AiOutlineUser /></span>
+          <span className='search-btn'><AiOutlineSearch /></span>
+          <span className='cart'><SlBag /></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
